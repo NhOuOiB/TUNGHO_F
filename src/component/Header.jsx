538a8654fa8df7253422656a41bab96f4c41745c
mike.chang <s33165309000@gmail.com>
2023-07-20 14:40:46 +0800

    [feat] tungho logo

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '/src/assets/logo.gif';
 
 const Header = () => {
   const [link] = useState([
@@ -21,21 +22,27 @@ const Header = () => {
     },
   ]);
   return (
-    <div className="bg-white text-[#444] h-12 flex justify-start items-center px-10">
-      {link.map((v, i) => {
-        return (
-          <div
-            className={`w-20 h-full font-bold relative hover:text-cyan-700 hover:cursor-pointer hover:bg-slate-50 ${
-              i == 0 ? 'border-x' : 'border-e'
-            }`}
-            key={i}
-          >
-            <Link to={`${v.link}`} className="w-full h-full flex justify-center items-center">
-              {v.name}
-            </Link>
-          </div>
-        );
-      })}
+    <div className="bg-white text-[#444] h-12 flex">
+      <div className="">
+        <img className="w-20 h-20 bg-white rounded-full p-1" src={logo} alt="" />
+      </div>
+      <div className="flex items-center">
+      <div className='ms-2 me-4 font-extrabold text-xl'>東河紡織</div>
+        {link.map((v, i) => {
+          return (
+            <div
+              className={`w-20 h-full font-bold relative hover:text-cyan-700 hover:cursor-pointer hover:bg-slate-50 ${
+                i == 0 ? 'border-x' : 'border-e'
+              }`}
+              key={i}
+            >
+              <Link to={`${v.link}`} className="w-full h-full flex justify-center items-center">
+                {v.name}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
